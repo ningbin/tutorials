ad8ca91f81a46fc88e9aac89538214f8d4e9603b
i322711 <bin.ning@sap.com>
2019-08-08 15:48:42 +0800

    demo version

---
+++ spring-boot-bootstrap/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Book(id,title,author) values(9001,'Cloud Native Java','Josh Long & Kenny Bastani');
+insert into Book(id,title,author) values(9002,'Cloud Foundry Definitive Guide','Duncan C. E. Winn');
+insert into Book(id,title,author) values(9003,'Cloud Foundry for Developers','Rick Farmer & Rahul Jain & David Wu');
